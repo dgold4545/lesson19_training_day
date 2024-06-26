3e06e818feab16ad9ecebf880f64e5aee40187a6
DGOLD7788 <80936904545dgold@gmail.com>
2024-06-26 12:18:09 +0300

    well done

--- script.js
+++ script.js
@@ -41,25 +41,25 @@
 
 // Приклад використання setInterval та setTimeout
 // Створимо блок з рекламою яка зникне через 10 секунд
-const elements = {
-  content: document.querySelector(".js-content"),
-  text: document.querySelector(".js-text"),
-};
+// const elements = {
+//   content: document.querySelector(".js-content"),
+//   text: document.querySelector(".js-text"),
+// };
 
-let counter = 10;
+// let counter = 10;
 
-elements.text.textContent = `Залишилось ${counter} секунд`;
+// elements.text.textContent = `Залишилось ${counter} секунд`;
 
-const id = setInterval(() => {
-  counter -= 1;
+// const id = setInterval(() => {
+//   counter -= 1;
 
-  if (!counter) {
-    clearInterval(id);
-    elements.content.style.display = "none";
-  }
+//   if (!counter) {
+//     clearInterval(id);
+//     elements.content.style.display = "none";
+//   }
 
-  elements.text.textContent = `Залишилось ${counter} секунд`;
-}, 1000);
+//   elements.text.textContent = `Залишилось ${counter} секунд`;
+// }, 1000);
 
 // ***********Синхронний та асинхронний JavaScript*********** \\
 // console.log("1");
@@ -91,6 +91,10 @@ const id = setInterval(() => {
 //   console.log(targetDate);
 
 //   const diff = targetDate - currentDate;
+// console.log('minutes', diff / 1000 /60 / 60 / 24)
+// console.log(targetDate - currentDate);
+
+// console.log(targetDate)
 
 //   console.log(diff);
 // }, 1000);
@@ -123,6 +127,39 @@ const namesOfMonth = [
 
 const elements = {
   day: document.querySelector(".date-day"),
-  date: document.querySelector('.date'),
-  month: 
-}
+  date: document.querySelector(".date"),
+  month: document.querySelector(".date-month"),
+  year: document.querySelector(".date-year"),
+  clock: document.querySelector(".digital-clock"),
+  seconds: document.querySelector(".clock-seconds__arrow"),
+  minutes: document.querySelector(".clock-minutes__arrow"),
+  hours: document.querySelector(".clock-hours__arrow"),
+};
+
+setInterval(() => {
+  const currentTime = new Date();
+
+  const day = currentTime.getDay();
+  const date = currentTime.getDate();
+  const month = currentTime.getMonth();
+  const year = currentTime.getFullYear();
+  const seconds = currentTime.getSeconds();
+  const minutes = currentTime.getMinutes();
+  const hours = currentTime.getHours();
+  const milliseconds = currentTime.getMilliseconds();
+  const localTime = currentTime.toLocaleTimeString("uk-UA");
+
+  const secondsDeg = (360 / 60) * seconds; // + (360 / 60 / 1000) * milliseconds
+  const minutesDeg = (360 / 60) * minutes + (360 / 60 / 60) * seconds;
+  const hoursDeg = (360 / 12) * hours + (360 / 12 / 60) * minutes;
+
+  elements.day.textContent = arrDay[day];
+  elements.date.textContent = date;
+  elements.month.textContent = namesOfMonth[month];
+  elements.year.textContent = year;
+  elements.clock.textContent = `Поточний час ${localTime}`;
+
+  elements.seconds.style.transform = `rotate(${secondsDeg}deg)`;
+  elements.minutes.style.transform = `rotate(${minutesDeg}deg)`;
+  elements.hours.style.transform = `rotate(${hoursDeg}deg)`;
+}, 1000);
